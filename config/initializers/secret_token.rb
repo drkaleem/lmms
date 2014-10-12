6adee8e411b1d15bc9a679eaad4580fda09b4ca4
Kaleem Ahned <drkaleem@gmail.com>
2014-10-12 17:27:48 -0400

    Init Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lmms::Application.config.secret_key_base = '02786f549890bd4a8185f3c199d807b4c0336263976009f8820d34bc4fcff3b6d1bcea6f29958aa9fccc68f8bc9df6443b4951ed504f28c9ce4b111a8d6a4c90'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lmms::Application.config.session_store :cookie_store, key: '_lmms_session'
